849931ffb3a31a5a61deacffc5cc867c28e55280
JJ <jj.vega86@gmail.com>
2021-10-12 08:44:44 -0400

    Added simple modal to CharacterViewer component

--- src/components/CharacterViewer/characterViewer.jsx
+++ src/components/CharacterViewer/characterViewer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
 const style = {
   margin: "6px 0px 6px 0px",
@@ -10,11 +11,30 @@ const style = {
   background: "rgba(76, 175, 80, 0.3)",
 };
 
+//TODO: Add modal to view Character's birth year on a button click
+//TODO: Refactor for a single use Modal component that can be used anywhere
 const CharacterViewer = ({ name, birthYear }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const showModal = () => setIsOpen(true);
+  const hideModal = () => setIsOpen(false);
   return (
     <div style={style}>
       <h1>{name}</h1>
-      <h3>{birthYear}</h3>
+      <button onClick={showModal} className="btn btn-success">
+        Click to view birth year!
+      </button>
+      <Modal show={isOpen}>
+        <Modal.Header>More Info for {name}</Modal.Header>
+        <Modal.Body>Their birth year is {birthYear}</Modal.Body>
+        <button
+          style={{ width: "100px", justifyContent: "center" }}
+          onClick={hideModal}
+          className="btn btn-success"
+        >
+          Close
+        </button>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
     </div>
   );
 };
